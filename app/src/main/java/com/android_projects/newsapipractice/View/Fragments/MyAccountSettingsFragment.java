7aeb0ee8207822e4dccce55748ad4cf0294d490f
Shu Zhang <shu@goopter.com>
2020-08-13 19:23:03 -0700

    Almost finished settings UI

--- app/src/main/java/com/android_projects/newsapipractice/View/Fragments/MyAccountSettingsFragment.java
+++ app/src/main/java/com/android_projects/newsapipractice/View/Fragments/MyAccountSettingsFragment.java
@@ -35,6 +35,7 @@ public class MyAccountSettingsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setSettingsUI();
         logoutBtn();
     }
 
@@ -47,5 +48,26 @@ public class MyAccountSettingsFragment extends Fragment {
                 startActivity(intent);
             });
         });
+    }
+
+    private void setSettingsUI(){
+        settingBinding.settingsWifi.prefSettingTitle.setText("Use Wi-Fi (Default)");
+        settingBinding.settingsWifi.prefSettingDescription.setText("Default setting that allows user to enable Wi-Fi");
+        settingBinding.settingsWifi.prefSettingSwitch.setEnabled(true);
+        settingBinding.settingsWifi.prefSettingListImgView.setImageResource(R.mipmap.ic_wifi);
+
+        settingBinding.settingsMobileData.prefSettingTitle.setText("Use Cellular Data");
+        settingBinding.settingsMobileData.prefSettingListImgView.setImageResource(R.mipmap.ic_cellular_network);
+        settingBinding.settingsMobileData.prefSettingDescription.setText("Alow user to enable mobile network");
+
+        //May change font size tab ui later
+        settingBinding.settingFontSize.prefSettingTitle.setText("Font Size");
+        settingBinding.settingFontSize.prefSettingDescription.setText("Allow user to change text size");
+        settingBinding.settingFontSize.prefSettingListImgView.setImageResource(R.mipmap.ic_font_size);
+
+        settingBinding.settingsTextFont.prefSettingTitle.setText("Font");
+        settingBinding.settingsTextFont.prefSettingDescription.setText("Allow user to change text font");
+        settingBinding.settingsTextFont.prefSettingListImgView.setImageResource(R.mipmap.ic_font);
+
     }
 }
