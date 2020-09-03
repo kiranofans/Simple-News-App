cd129774a9f2b0670418b30eb53163bf9bd5932c
Shu Zhang <shu@goopter.com>
2020-09-03 14:58:11 -0700

    show font size dialog with AlertDialog.Builder

--- app/src/main/java/com/android_projects/newsapipractice/View/Fragments/MyAccountSettingsFragment.java
+++ app/src/main/java/com/android_projects/newsapipractice/View/Fragments/MyAccountSettingsFragment.java
@@ -1,22 +1,30 @@
 package com.android_projects.newsapipractice.View.Fragments;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import com.android_projects.newsapipractice.R;
 import com.android_projects.newsapipractice.View.LoginActivity;
-import com.android_projects.newsapipractice.View.MyAccountActivity;
+import com.android_projects.newsapipractice.databinding.DialogFontSizeBinding;
 import com.android_projects.newsapipractice.databinding.FragmentAccountSettingBinding;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.radiobutton.MaterialRadioButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.android_projects.newsapipractice.View.LoginActivity.googleSignInClient;
 
@@ -24,20 +32,67 @@ public class MyAccountSettingsFragment extends Fragment {
     private final String TAG = MyAccountSettingsFragment.class.getSimpleName();
 
     private FragmentAccountSettingBinding settingBinding;
+    private DialogFontSizeBinding dialogBinding;
+    private AlertDialog.Builder dialog;
+
     private View v;
+    private RadioGroup fontSizeRadioGroup;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         settingBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_account_setting, container, false);
-        return settingBinding.getRoot();
+        dialogBinding = DataBindingUtil.inflate(inflater.from(getContext()), R.layout.dialog_font_size, null, false);
+        fontSizeRadioGroup = dialogBinding.dialogRadioGroup;
+
+        return v = settingBinding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         setSettingsUI();
+        settingsTabOnClick();
         logoutBtn();
+    }
+
+    private void settingsTabOnClick() {
+        settingBinding.settingFontSize.prefSettingsLinearLayout.setOnClickListener((View v) -> {
+            showFontSizePopup();
+        });
+    }
+
+    private void onRadioButtonChecked() {
+        //for dialog not alert dialog
+        fontSizeRadioGroup.setOnCheckedChangeListener((RadioGroup radioGroup, int checkedId) -> {
+            int childCount = radioGroup.getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                MaterialRadioButton materialRb = (MaterialRadioButton) radioGroup.getChildAt(i);
+                if (materialRb.getId() == checkedId) {
+                    Log.d(TAG, "Selected radio button -> " + materialRb.getText().toString());
+                }
+            }
+        });
+    }
+
+    @SuppressLint("ResourceType")
+    private void showFontSizePopup() {
+        String[] items = {"Small", "Medium", "Large", "Extra Large"};
+        fontSizeRadioGroup.setVisibility(View.VISIBLE);
+
+        dialog = new AlertDialog.Builder(v.getContext()).setTitle("Select A Size")
+                .setCancelable(false);
+        dialog.setSingleChoiceItems(items, -1, (DialogInterface dialogInterface, int item) -> {
+            for (int i = 0; i < items.length; i++) {
+                if (i == item) {
+                    Log.d(TAG, "Selected radio button -> " + item);
+                }
+            }
+            dialogInterface.dismiss();
+        });
+        dialog.show();
+
+        Log.d(TAG, "Clicked");
     }
 
     private void logoutBtn() {
@@ -52,7 +107,7 @@ public class MyAccountSettingsFragment extends Fragment {
         });
     }
 
-    private void setSettingsUI(){
+    private void setSettingsUI() {
         settingBinding.settingsWifi.prefSettingTitle.setText(getString(R.string.settings_title_wifi));
         settingBinding.settingsWifi.prefSettingDescription.setText(getString(R.string.settings_description_wifi));
         settingBinding.settingsWifi.prefSettingSwitch.setEnabled(true);
